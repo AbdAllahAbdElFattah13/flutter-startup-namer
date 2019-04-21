d0b60c39c433e3c1e8fe947087899f09319d478d
AbdAllah <AbdAllah.abdelfattah@thed.io>
2019-04-21 11:15:28 +0200

    Change app theme color.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Startup Name Generator', home: RandomWords());
+    return MaterialApp(
+        title: 'Startup Name Generator',
+        theme: ThemeData(
+          // Add the 3 lines from here...
+          primaryColor: Colors.white,
+        ),
+        home: RandomWords());
   }
 }
 
